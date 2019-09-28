73c3ae9cf82b9a390f840a8cfb747485ad25bc1b
Jonathan Duck <duckbrain30@gmail.com>
2019-09-28 05:15:14 -0700

    Add tests to ensure old behavior

---
+++ src/helpers.test.js
@@ -0,0 +1,19 @@
+var helpers = require('./helpers.js')
+
+test('pathToHost', function() {
+    var pathToHost = helpers.pathToHost;
+    expect(pathToHost('./dir/example.com/email@example.com', 'example.com')).toBe('example.com')
+    expect(pathToHost('./dir/example.com/sub.example.com', 'example.com')).toBe('sub.example.com')
+    expect(pathToHost('pi.hole', 'pi.hole')).toBe('pi.hole')
+    expect(pathToHost('pi.hole', 'local.pi.hole')).toBe('pi.hole')
+    expect(pathToHost('email@example.com', 'example.com')).toBe(null)
+    expect(pathToHost('example.com', 'example.com:8080')).toBe('example.com')
+    // future behavior
+    // expect(pathToHost('example.com:8080', 'example.com:8080')).toBe('example.com:8080')
+    // expect(pathToHost('example.com:1234', 'example.com:8080')).toBe(null)
+    // expect(pathToHost('example.com:8080', 'example.com')).toBe(null)
+    // current behavior
+    expect(pathToHost('example.com:8080', 'example.com:8080')).toBe('example.com:8080')
+    expect(pathToHost('example.com:1234', 'example.com:8080')).toBe('example.com:1234')
+    expect(pathToHost('example.com:8080', 'example.com')).toBe('example.com:8080')
+})
