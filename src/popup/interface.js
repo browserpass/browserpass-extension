6f0d5bb1d440caa3fd5368bf54f692799340ea44
Steve Gilberd <steve@erayd.net>
2020-06-03 12:28:45 +1200

    Add hotkeys to tooltips

--- src/popup/interface.js
+++ src/popup/interface.js
@@ -81,7 +81,7 @@ function view(ctl, params) {
                         onkeydown: keyHandler.bind(result)
                     },
                     [
-                        m("div.name", [
+                        m("div.name", { title: "Fill username / password | <ENTER>" }, [
                             m("div.line1", [
                                 m(
                                     "div.store.badge",
@@ -108,12 +108,12 @@ function view(ctl, params) {
                         ]),
                         m("div.action.copy-password", {
                             tabindex: 0,
-                            title: "Copy password",
+                            title: "Copy password | <CTRL+C>",
                             action: "copyPassword"
                         }),
                         m("div.action.copy-user", {
                             tabindex: 0,
-                            title: "Copy username",
+                            title: "Copy username | <CTRL+SHIFT+C>",
                             action: "copyUsername"
                         })
                     ]
